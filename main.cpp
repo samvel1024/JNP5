99d44497a3c2556ac1be7e09f3fe56644e79ee6c
Piotr Karpiński <pk394334@students.mimuw.edu.pl>
2018-12-18 21:45:19 +0100

    Added friend operator.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "CitationGraph.h"
+#include <set>
+#include <memory>
 
 template <typename T>
 class Publication {
@@ -17,7 +19,41 @@ public:
 };
 
 
+class Dummy {
+private:
+    int id;
+
+public:
+
+    Dummy(int arg) : id(arg) {}
+
+    bool operator<(const Dummy& lhs) const {
+      return id < lhs.id;
+    }
+};
+
+/*bool operator<(const std::weak_ptr<Dummy>& lhs, const std::weak_ptr<Dummy>& rhs) {
+  return lhs.lock()->operator<(*rhs.lock());
+}*/
+
 int main() {
-    CitationGraph<Publication<int>> graph(23);
+  //std::set<std::weak_ptr<Dummy>> set_weak;
+  std::set<std::shared_ptr<Dummy>> set_shared;
+
+  auto ptr2 = std::make_shared<Dummy>(2);
+  auto ptr3 = std::make_shared<Dummy>(3);
+
+/*  set_weak.emplace(ptr2);
+  set_weak.emplace(ptr3);*/
+
+  set_shared.emplace(ptr2);
+  set_shared.emplace(ptr3);
+
+
+
+  //CitationGraph<Publication<int>> graph(23);
+
+
+
     return 0;
 }
